d9adce24b0084d923394d3922cec80e0eea1a323
voldezhur <voldezhur@gmail.com>
2024-04-03 18:32:45 +0300

    added meta info about character

--- client/src/components/CharacterSheet.jsx
+++ client/src/components/CharacterSheet.jsx
@@ -9,11 +9,42 @@ function CharacterName (props) {
     );
 }
 
+function CharacterInfo (props) {
+    return (
+        <div class="character-info">
+            <div id="content">
+                <div>{props.content}</div>
+                <div>{props.value}</div>
+            </div>
+
+            <div id="info">{props.title}</div>
+        </div>
+    );
+}
+
 export default function CharacterSheet () {
     return (
         <div id="character-sheet">
-            <span id="character-sheet-title">Лист Персонажа</span>
-            <CharacterName content="Моргрим" title="Имя персонажа" />
+            <div id="character-sheet-title">Лист Персонажа</div>
+
+            <div id="character-meta">
+                <CharacterName content="Моргрим" title="Имя персонажа" />
+
+                <div id="character-about">
+                    <div id="character-about-row">
+                        <CharacterInfo content="Плут" value="1" title="Класс и уровень" />
+                        <CharacterInfo content="Бездомный" title="Предыстория" />
+                        <CharacterInfo content="Имя" title="Имя игрока" />
+                    </div>
+                    <div id="character-about-row">
+                        <CharacterInfo content="Полурослик" title="Раса" />
+                        <CharacterInfo content="Chaotic Neutral" title="Мировоззрение" />
+                        <CharacterInfo content="100 оч." title="Опыт" />
+                    </div>
+                </div>
+            </div>
+
+            
         </div>
     );
 }

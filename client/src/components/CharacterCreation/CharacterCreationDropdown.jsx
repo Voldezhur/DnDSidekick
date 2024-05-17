f19f1011528c68d4fa8cf3bb5cadda7953a5fc68
Voldezhur <voldezhur@gmail.com>
2024-05-17 11:28:02 +0300

    Added a blank option in the character creation dropdown
    notes:
    . going to work on polishing some aspects of the website, mainly the styling. After that will work on making groups and the main functionality

--- client/src/components/CharacterCreation/CharacterCreationDropdown.jsx
+++ client/src/components/CharacterCreation/CharacterCreationDropdown.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 const CharacterCreationDropdown = ({ title, setProperty, optionsList }) => {
+    const newOptions = ['', ...optionsList];
+    
     return (
         <>
             <div className='input-form'>
                 <label className="input-inside-flex">
                     {title}:
                     <select onChange={(e) => {setProperty(e.target.value)}}>
-                        {optionsList.map((option, i) => {
+                        {newOptions.map((option, i) => {
                             return (
                                 <option key={i} value={option.id}>{option.name}</option>
                             );

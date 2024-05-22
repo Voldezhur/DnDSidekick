b8c82bde97db76c6f639a7a9cc7b0a674b192da4
Voldezhur <voldezhur@gmail.com>
2024-05-22 18:37:36 +0300

    Minor fixes and changes in CharacterSheet . fixex the error when using useFetching - the cause was the character not being loaded yet . translated the attributes

--- client/src/components/CharacterPage/CharacterSheet.jsx
+++ client/src/components/CharacterPage/CharacterSheet.jsx
@@ -1,27 +1,51 @@
 // Импорт функционала
-import { React, useEffect, useState } from "react";
+import { React, useState } from "react";
 import useFetching from "../../hooks/useFetching";
 
 // Импорт компонентов
 import CharacterSheetPageSelector from "./CharacterSheetPageSelector";
 
 
+const shittyAttributeTranslation = (attribute) => {
+    switch(attribute) {
+        case "strength":
+            return "Сила";
+        case "dexterity":
+            return "Ловкость";
+        case "constitution":
+            return "Телосложение";
+        case "intelligence":
+            return "Интеллект";
+        case "wisdom":
+            return "Мудрость";
+        case "charisma":
+            return "Харизма";
+        default:
+            return attribute;
+    }
+}
+
 const CharacterSheet = ({ character, isLoading }) => {
     const [page, setPage] = useState(1);
-    const [fetchRace, race, isRaceLoading] = useFetching("http://localhost:8000/compendium/races/getRaceById/" + character.race_id);
-    const [fetchClass, characterClass, isClassLoading] = useFetching("http://localhost:8000/compendium/classes/getClassById/" + character.class_id);
+    const [fetchRace, race] = useFetching("http://localhost:8000/compendium/races/getRaceById/" + character.race_id);
+    const [fetchClass, characterClass] = useFetching("http://localhost:8000/compendium/classes/getClassById/" + character.class_id);
 
-    useEffect(() => {
-        fetchRace();
-        fetchClass();
-    }, [])
-
+    const [fetched, setFetched] = useState(false);
 
     const renderPage = () => {
         if (isLoading) {
             return ("Загрузка");    
         }
         else {
+            // Для того, чтобы фетчилось только один раз
+            // В useEffect не получилось, потому что в начале рендера персонаж еще не подгружен
+            if (!fetched)
+            {
+                fetchRace();
+                fetchClass();
+                setFetched(true);
+            }
+
             switch (page) {
                 // Первая страница - основное
                 case 1:
@@ -30,20 +54,16 @@ const CharacterSheet = ({ character, isLoading }) => {
                             <CharacterSheetPageSelector title={"Основное"} page={page} setPage={setPage} />
                             <div className="character-sheet-flex">
                                 <div className="character-sheet-entry">
-                                    <p>Раса: {character.race_id}</p>
-                                    {isRaceLoading &&
-                                        <p>Раса: {race.map((item, i) => {return(<div key={i}>{item.name}</div>)})}</p>
-                                    }
+                                    <p>Раса: {race.map((item) => {return(item.name)})}</p>
 
-                                    <p>Класс: {character.class_id}</p>
-                                    {/* <p>Класс: {characterClass.map((item, i) => {return(<div key={i}>{item.name}</div>)})}</p> */}
+                                    <p>Класс: {characterClass.map((item) => {return(item.name)})}</p>
 
                                 </div>
 
                                 <div className="character-sheet-entry">
                                     {Object.entries(character.ability_scores).map((item, i) => {
                                         return (
-                                                <div key={i}>{item[0]}: {item[1].score} modifier: {item[1].modifier}</div>
+                                                <div key={i}>{shittyAttributeTranslation(item[0])}: {item[1].score} Модификатор: {item[1].modifier}</div>
                                         );
                                     })}
                                 </div>
